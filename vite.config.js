97b17808ecbc16d60bdca5cfa6b17054dd05210c
fenrirheimr <xhtmlblog@gmail.com>
2023-12-20 12:29:53 +0500

    add dotenv & dotenv-expand

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,56 @@
-import { defineConfig } from 'vite'
+import dotenvExpand from 'dotenv-expand';
+import { loadEnv, defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import webfontDownload from 'vite-plugin-webfont-dl';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    webfontDownload([
-      'https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@300;400;500;600;700&display=swap'
-    ])
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/_mixins.scss";`
+export default defineConfig(({ mode }) => {
+  // This check is important!
+  if (mode === 'development') {
+    const env = loadEnv(mode, process.cwd(), '');
+    dotenvExpand.expand({ parsed: env });
+  }
+
+  return {
+    plugins: [
+      vue(),
+      webfontDownload([
+        'https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@300;400;500;600;700&display=swap'
+      ])
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/styles/_mixins.scss";`
+        }
       }
     }
-  }
-})
+  };
+});
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     webfontDownload([
+//       'https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@300;400;500;600;700&display=swap'
+//     ])
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: `@import "./src/styles/_mixins.scss";`
+//       }
+//     }
+//   }
+// })
