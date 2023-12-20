8af928eb5663d7ca5a2f8f435ad75c9f8454cf9f
fenrirheimr <xhtmlblog@gmail.com>
2023-12-21 01:20:53 +0500

    restore vercel.json

--- vite.config.js
+++ vite.config.js
@@ -20,9 +20,6 @@ export default defineConfig(({ mode }) => {
         'https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@300;400;500;600;700&display=swap'
       ])
     ],
-    vercel: {
-      "rewrites": [{ "source": "/(.*)", "destination": "/" }]
-    },
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
