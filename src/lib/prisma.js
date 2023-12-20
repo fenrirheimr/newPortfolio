41d665e43ee4c7a241217a7f00474da47887aecd
fenrirheimr <xhtmlblog@gmail.com>
2023-12-21 02:43:53 +0500

    test

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -6,13 +6,15 @@ const global = {
 
 let prisma
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
+if (typeof window === "undefined") {
+  if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient()
+  } else {
+    if (!global.prisma) {
+      global.prisma = new PrismaClient()
+    }
+    prisma = global.prisma
   }
-  prisma = global.prisma
 }
 
 export default prisma
